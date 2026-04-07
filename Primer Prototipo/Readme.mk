5b29f7856ceb81b26f86998c03c87a0edb82f4a9
Jorge Angel Garcia Alvarado <jorge.garciaa@uanl.edu.mx>
2026-04-07 16:54:54 -0600

    Add README for muscle contraction project prototype
    Added a README file for the first prototype of the muscle contraction project, detailing the components used.

---
+++ Primer Prototipo/Readme.mk
@@ -0,0 +1,6 @@
+#Agregar readme
+
+
+
+Esa es el primer prototipo d emi proyecto de Contracciones musculares donde utilizo un arduino uno, y dos pic16f84a un ad8232 un display lcd 16x2 alambre estanado
+electrodos de gel un led rgb catodo comun un un buzzer 
